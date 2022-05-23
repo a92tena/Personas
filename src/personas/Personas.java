55f4d0099b9720220a3b9bd905ccedae4e1042e4
a92tena <albertotena_92@hotmail.com>
2022-05-23 12:55:15 +0200

    primer commit

---
+++ src/personas/Personas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package personas;
+
+/**
+ *
+ * @author Tena Padre
+ */
+public class Personas {
+
+    static String nombre = "Alberto";
+    static int edad = 29;
+    static String modulo = "Entorno de desarrolos";
+    public static void main(String[] args) {
+        
+         System.out.println("El nombre del alumno es: "+nombre);
+         
+         System.out.println("La edad del alumno es: "+edad);
+         
+         System.out.println("El modulo del examen es: "+modulo);
+    }
+    
+}
